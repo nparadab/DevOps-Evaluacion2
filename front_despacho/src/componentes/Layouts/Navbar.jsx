9a9c038d89257224a1e5d5d593fc7738020ffc05
nparadab <n.paradab@duocuc.cl>
2026-06-03 20:49:02 -0400

    Update Navbar link text from 'Usuarios222' to 'Usuarios'

--- front_despacho/src/componentes/Layouts/Navbar.jsx
+++ front_despacho/src/componentes/Layouts/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             href="#"
             className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
           >
-            Usuarios222 
+            Usuarios
           </a>
         </li>
         <li>
